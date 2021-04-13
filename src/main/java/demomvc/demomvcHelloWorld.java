9998edbf2601da8738d65446c83548340991925f
amusher <motions9876@gmail.com>
2021-04-13 12:19:07 -0500

    Main page working

---
+++ src/main/java/demomvc/demomvcHelloWorld.java
@@ -0,0 +1,17 @@
+package demomvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class demomvcHelloWorld {
+	
+	@RequestMapping("/welcome")
+	public ModelAndView helloWorld() {
+	String message = "<br><div style='text-align:center;'>"
+			+ "<h3>********** Hello World, Spring MVC Tutorial</h3>This message is coming from demomvcHelloworld.java **********</div><br><br>";
+	return new ModelAndView("welcome", "message", message);
+
+	}
+}
